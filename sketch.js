a8d84444e1d5fc1ece471aea84a3ed939aa2326c
Satvik435 <86778274+Satvik435@users.noreply.github.com>
2021-07-08 16:25:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
 var weight = [35,38,42,45,43,34,36,41,48,32];
-
+var sum = 0
 function setup() {
   createCanvas(400,400);
+for(var i = 0;i < marks.length; i++1){
 
+sum = sum + marks[i]
+}
+  var avg = sum/marks.length 
+ console.log (avg) ;
+}
 }
 
 function draw() 
